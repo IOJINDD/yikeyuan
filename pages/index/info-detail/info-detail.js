9949f72f3175ac708ad6d8ed3927cd5dd11b578a
IOJINDD <617638699@qq.com>
2018-07-04 16:14:25 +0800

    修复安卓兼容问题

--- pages/index/info-detail/info-detail.js
+++ pages/index/info-detail/info-detail.js
@@ -93,5 +93,12 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  statechange(e) {
+    console.log('live-player code:', e.detail.code)
+  },
+  error(e) {
+    console.error('live-player error:', e.detail.errMsg)
   }
 })
